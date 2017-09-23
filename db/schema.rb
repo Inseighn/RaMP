7a7774b3dc9c558bd922b33e8d12481af07cdf63
Aaron Rubesh <adrubesh@uh.edu>
2017-09-23 11:07:47 -0500

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170923012721) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.string "ps"

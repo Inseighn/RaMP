507d2ad66a4a7c519d95cf313684f3676156f3f2
Aaron Rubesh <adrubesh@uh.edu>
2017-09-24 11:10:39 -0500

    Created rails associations between payments/members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,4 @@
 class Member < ApplicationRecord
+	has_many :payments
+	has_many :semesters, through: :payments 
 end
--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,2 +1,4 @@
 class Semester < ApplicationRecord
+	has_many :payments
+	has_many :members, through: :payments
 end
